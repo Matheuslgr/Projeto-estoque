3e691b9a8e5d6158d8578b49003b6d9433bcf9d6
Matheus Larangeira <matheus-larangeira@hotmail.com>
2025-11-07 20:52:28 -0300

    add rota home

--- back-end/main.py
+++ back-end/main.py
@@ -1,1 +1,8 @@
 from fastapi import FastAPI
+import funcao
+
+app = FastAPI(title="Gerenciador de produtos")
+
+@app.get("/")
+def home():
+    return{"mensagem": "Bem-vindo ao gerenciador de produtos"}
