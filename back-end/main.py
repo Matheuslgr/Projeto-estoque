442b03b974a25b999210eae040db78bb1834b96c
Matheus Larangeira <matheus-larangeira@hotmail.com>
2025-11-07 21:00:16 -0300

    add rota cadastrar produto

--- back-end/main.py
+++ back-end/main.py
@@ -6,3 +6,8 @@ app = FastAPI(title="Gerenciador de produtos")
 @app.get("/")
 def home():
     return{"mensagem": "Bem-vindo ao gerenciador de produtos"}
+
+@app.post("/produtos")
+def adicionar_produto(nome: str, categoria: str, preco: float, quantidade: float):
+    funcao.cadastrar_produto(nome, categoria, preco, quantidade)
+    return {"200": "Produto cadastrado com sucessso!"}
